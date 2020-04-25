5c572400c1eb37a92fdb1d6ee16e1defbfa386f7
gabschlam <gabschlam@gmail.com>
2020-04-25 12:51:38 -0500

    Componentes agregados

---
+++ charlieAngular/src/app/main-components/image-carrousel/image-carrousel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-image-carrousel',
+  templateUrl: './image-carrousel.component.html',
+  styleUrls: ['./image-carrousel.component.scss']
+})
+export class ImageCarrouselComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
