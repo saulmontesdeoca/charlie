23c956386ed5131ddfb524452a1c8e82c54a4483
gabschlam <gabschlam@gmail.com>
2020-04-25 11:36:12 -0500

    Base NavBar y Footer

--- charlieAngular/src/app/app.module.ts
+++ charlieAngular/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './main-components/nav-bar/nav-bar.component';
+import { FooterComponent } from './main-components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
