e14e8b4aaccd6e93b273317323d57a9c6abebbac
Saul Montes De Oca <mr.sauce97@gmail.com>
2020-05-06 20:22:12 -0500

    Reparando problema con footers y navbars

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,46 +4,39 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavBarComponent } from './main-components/nav-bar/nav-bar.component';
-import { FooterComponent } from './main-components/footer/footer.component';
 import { HomeComponent } from './modules/comprador/home/home.component';
-import { ImageCarrouselComponent } from './main-components/image-carrousel/image-carrousel.component';
 import { NewsFeedComponent } from './modules/info/news-feed/news-feed.component';
 import { ContactoComponent } from './modules/info/contacto/contacto.component';
 import { PerfilComponent } from './modules/shared/perfil/perfil.component';
 import { AgregarComponent } from './modules/vendedor/agregar/agregar.component';
 import { EditarComponent } from './modules/vendedor/editar/editar.component';
-import { FormProductoComponent } from './main-components/form-producto/form-producto.component';
 import { ComprarComponent } from './modules/comprador/comprar/comprar.component';
 import { LogInComponent } from './modules/shared/log-in/log-in.component';
 import { CrearCuentaComponent } from './modules/shared/crear-cuenta/crear-cuenta.component';
 import { AdminModule } from './modules/admin/admin.module';
+import { MainComponentsModule } from './main-components/main-components.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
-    FooterComponent,
     HomeComponent,
-    ImageCarrouselComponent,
     NewsFeedComponent,
     ContactoComponent,
     PerfilComponent,
     AgregarComponent,
     EditarComponent,
-    FormProductoComponent,
     ComprarComponent,
     LogInComponent,
-    CrearCuentaComponent
+    CrearCuentaComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    AdminModule
-
+    AdminModule,
+    MainComponentsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/modules/comprador/comprador.module.ts
+++ src/app/modules/comprador/comprador.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { MainComponentsModule } from 'src/app/main-components/main-components.module';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, MainComponentsModule],
 })
-export class CompradorModule { }
+export class CompradorModule {}
