ba59af843b9df27cf2001513a3eb304d4a72bcfd
Diego Acevedo <diegomx2@hotmail.com>
2020-05-08 21:48:00 -0500

    Home con datos del JSON pero aun sin servicio

--- src/app/modules/comprador/home/home.component.ts
+++ src/app/modules/comprador/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import  *  as  data  from '../../../../data/data.json'
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  datos:  any  = (data  as  any).default;
 
-  constructor() { }
+  constructor() { 
+
+  }
 
   ngOnInit(): void {
+    console.log(this.datos);
   }
 
 }
