df8bf43ff2d81092f00dcf3b69d01fe14e908c86
Charlie dlg <charliedelagarza@gmail.com>
2020-06-11 10:10:18 -0500

    Chatbot

---
+++ src/app/main-components/chatbot/chatbot.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-chatbot',
+  templateUrl: './chatbot.component.html',
+  styleUrls: ['./chatbot.component.scss']
+})
+export class ChatbotComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/main-components/main-components.module.ts
+++ src/app/main-components/main-components.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-
+import { ChatbotComponent } from './chatbot/chatbot.component';
 import { FooterComponent } from './footer/footer.component';
 import { FormProductoComponent } from './form-producto/form-producto.component';
 import { ImageCarrouselComponent } from './image-carrousel/image-carrousel.component';
@@ -11,12 +11,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule} from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+
 @NgModule({
   declarations: [
     FooterComponent,
     FormProductoComponent,
     ImageCarrouselComponent,
     NavBarComponent,
+    ChatbotComponent,
   ],
   imports: [CommonModule, RouterModule, MDBBootstrapModule.forRoot(), NgbModule],
   exports: [
@@ -26,6 +28,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     NavBarComponent,
     BrowserModule,
     HttpClientModule,
+    ChatbotComponent
   ],
 })
 export class MainComponentsModule {}
