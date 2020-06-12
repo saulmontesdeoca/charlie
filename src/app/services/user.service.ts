eecd9941ea5950cab1f61aff3ffa04d9290a5df2
Saul Montes De Oca <mr.sauce97@gmail.com>
2020-06-11 22:50:25 -0500

    Agregando estilos de chatbot y logo

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,42 +1,40 @@
 import { Injectable } from '@angular/core';
-import  *  as  data  from '../../data/data.json'
+import * as data from '../../data/data.json';
 import { Router } from '@angular/router';
-import {product} from '../interfaces/product'
-
+import { product } from '../interfaces/product';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
-  datos:  any  = (data  as  any).default;
+  datos: any = (data as any).default;
   products: any = data.products;
   users: any = data.users;
   user_products: any = this.users[0].products;
   product: product;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {}
 
   addProduct(datos: any) {
     this.products.push(datos);
     this.users[0].products.push(datos);
     this.router.navigateByUrl('/home');
   }
-  removeProduct(index: number){
-    this.users[0].products.splice(index,1);
+  removeProduct(index: number) {
+    this.users[0].products.splice(index, 1);
   }
-  findProduct(index){
-    this.product=this.user_products[index];
-    console.log(this.product)
+  findProduct(index) {
+    this.product = this.user_products[index];
+    console.log(this.product);
   }
 
-  updateProduct(id, name, description, price){
-
-    this.product=this.users[0].products.find(res => {
-      res.id==id;
+  updateProduct(id, name, description, price) {
+    this.product = this.users[0].products.find((res) => {
+      res.id == id;
     });
-    if(this.product){
+    if (this.product) {
       this.product.name = name;
-      this.product.description=description;
+      this.product.description = description;
       this.product.price = price;
     }
 
